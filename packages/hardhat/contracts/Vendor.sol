777bfa60fb1bb134c1f59e37f897f944c4ccd68e
Chandrasekhar Iyengar <chandrasekhariyengar@gmail.com>
2024-12-22 00:17:00 +0000

    mint tokens, send to vendor, able to withdraw too

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,16 +1,28 @@
 pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
-  // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+contract Vendor is Ownable {
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
   YourToken public yourToken;
+  uint256 public constant tokensPerEth = 100;
+
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
+  }
+
+  function buyTokens() public payable {
+    yourToken.transfer(msg.sender, msg.value * tokensPerEth);
+    emit BuyTokens(msg.sender, msg.value, msg.value * tokensPerEth);
+  }
+
+  function withdraw() public {
+    require(msg.sender == owner(), "Caller is not owner");
+    payable(owner()).transfer(address(this).balance);
   }
 
   // ToDo: create a payable buyTokens() function:
