c5c2b066704c36ab582b85a58a2b86ca0bf55285
Chandrasekhar Iyengar <chandrasekhariyengar@gmail.com>
2024-12-27 19:38:09 +0000

    fix prediction logic + add button to app

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -17,14 +17,19 @@ contract RiggedRoll is Ownable {
     }
 
     function riggedRoll() public {
+      require (
+        address(this).balance >= 0.002 ether
+      );
       bytes32 prevHash = blockhash(block.number - 1);
-      bytes32 hash = keccak256(abi.encodePacked(prevHash, address(this), diceGame.nonce()));
+      bytes32 hash = keccak256(abi.encodePacked(prevHash, address(diceGame), diceGame.nonce()));
       uint256 roll = uint256(hash) % 16;
-
-      console.log(roll);
+      console.log("\t", "   Dice Game Roll prediction:", roll);
 
       if (roll < 6) {
         diceGame.rollTheDice{value: 0.002 ether}();
+      }
+      else {
+        revert("Lost roll");
       }
     }
 
